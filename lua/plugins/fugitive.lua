a51866e5590cebd7e615b757e9334e8303b9e8c8
Ichi-Miike <mat.smith@epssw.com>
2024-08-23 08:05:35 +0100

    addd git support with fugitive... added beginnings of debugging

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,5 +2,7 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "<leader>gs", ":Git status<CR>", {})
+    vim.keymap.set("n", "<leader>gpush", ":Git push<CR>", {})
+    vim.keymap.set("n", "<leader>gpull", ":Git pull<CR>", {})
   end
 }
